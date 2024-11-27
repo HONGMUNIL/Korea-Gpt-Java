a125226b58632a2d202da5779e622af2867cd173
HONGMUNIL <bear4564@naver.com>
2024-11-27 12:31:17 +0900

    new

---
+++ src/chapter01/HelloWorld.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+
+//자바의 파일 단위
+//-클래스 파일을 생성해서 코드 작성: 파일명  UpperCamelCase 사용
+//파일의 확장자:.java
+
+//cf) UpperCamelCase
+// : 대문자로 시작, 단어와 단어의 연결은 대문자로 작성
+// 그외에는 소문자 작성
+public class HelloWorld {
+    public static void main(String[] args) {
+    //자동완성 단축키:ctrl + space
+        System.out.println("Hello World");
+        System.out.println("안냐세요 요로시꾸");
+        /*
+
+         */
+
+
+    }
+}
