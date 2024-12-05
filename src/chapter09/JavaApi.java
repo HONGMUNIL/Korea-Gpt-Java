3f65bc0bb615f783502bbd24364dbb7ca470b029
HONGMUNIL <bear4564@naver.com>
2024-12-05 10:57:14 +0900

    Object

---
+++ src/chapter09/JavaApi.java
@@ -0,0 +1,16 @@
+package chapter09;
+
+
+//API
+// : 소프트 웨어 또는 시스템 간 상호작용을 가늠하게 하는 정의된 메서드의 집합
+// : 소프트웨어나 하드웨어와 상호작용 할 수 있는 인터페이스를 제공
+
+public class JavaApi {
+
+    public static void main(String[] args) {
+        System.out.println();
+        //: System 클래스의 API를 호출하여 콘솔에 메시지를 출력
+
+    }
+
+}
---
+++ src/chapter09/JavaLang.java
@@ -0,0 +1,18 @@
+package chapter09;
+
+class MyClass{
+     MyClass(int id, String name){
+
+    }
+}
+
+
+
+public class JavaLang {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass(1,"이승아");
+        myClass.toString();
+    }
+
+    
+}
