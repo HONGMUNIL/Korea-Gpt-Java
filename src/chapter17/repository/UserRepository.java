840c150e608a6a4ad371603c35364ee3f30c977d
HONGMUNIL <bear4564@naver.com>
2024-12-17 09:07:20 +0900

    reservation

--- src/chapter17/repository/UserRepository.java
+++ src/chapter17/repository/UserRepository.java
@@ -11,17 +11,18 @@ public class UserRepository {
 
     public void save(User user) {
         users.add(user);
-
     }
 
-    public Optional<Object> findByUserId(String userId) {
-        //Optional 클래스 : 값이 존재 할수도 있고 존재하지 않을 수도 있는 객체를 감싸는 Wrapper 클래스
+
+    public Optional<User> findByUserId(String userId) {
+        // Optional 클래스: 값이 존재할 수도 있고 존재하지 않을 수도 있는 객체를 감싸는 Wrapper 클래스
         // +) ifPresentOrElse() 메서드
-        //      : Optional 객체에 대해 값이 존재할 경우 ifPresent 로직
+        //      : Optinal 객체에 대해 값이 존재할 경우 ifPresent 로직
         //          , 그렇지 않을 경우 orElse의 로직을 처리
-        //      ifPresentOrElse(data -> {데이터가 존재할 경우 로직, 존재하지 않을 경우 로직});
+        //    ifPresentOrElse(data -> {데이터가 존재할 경우 로직}, 존재하지 않을 경우 로직);
         return users.stream()
                 .filter(user -> user.getUserId().equals(userId)).findFirst();
     }
-    public  List<User> findAll() {return  new ArrayList<>(users);
+
+    public List<User> findAll() { return new ArrayList<>(users); }
 }
--- src/chapter17/service/UserService.java
+++ src/chapter17/service/UserService.java
@@ -2,10 +2,7 @@ package chapter17.service;
 
 public interface UserService {
     void registerUser(String userId, String password, String name, String email);
-
     void login(String userId, String password);
-
     void logout();
-
-
+    boolean isLoggedIn();
 }
